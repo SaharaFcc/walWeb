b63dc6012f990ef79ca3d74fce6f2893163668cb
SaharaFcc <48973117+SaharaFcc@users.noreply.github.com>
2021-02-02 18:45:49 +0800

    Add files via upload

---
+++ 官网首页加载.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="blog">
+    <div class="loadingbox">
+      <div class="loader-circle"></div>
+      <div class="loader-line-mask">
+        <div class="loader-line"></div>
+      </div>
+      <img src="@/assets/images/common/loading_logo.png" alt="" />
+    </div>
+    <vue-particles
+      color="#dedede"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="star"
+      :particleSize="1"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="50"
+      :moveSpeed="1"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    >
+    </vue-particles>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+};
+</script>
